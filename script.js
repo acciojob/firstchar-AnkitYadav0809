84796eda8abf6c0539f3d638b815b252d70525ff
Ankit Yadav <115349567+AnkitYadav0809@users.noreply.github.com>
2025-09-21 10:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	 let trimmedText = text.trim();
+    
+    // Step 2: Check if the trimmed string is empty
+    if (trimmedText.length === 0) {
+        return ''; // Return empty string
+    }
+    
+    // Step 3: Return the first character
+    return trimmedText.charAt(0);
 }
 
 // Do not change the code below
